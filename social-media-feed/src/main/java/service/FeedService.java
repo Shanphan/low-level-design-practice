8fe7fb8e968edd5a07af56b79eb492fe53376df9
Shanphan <shantanu.phanse189@gmail.com>
2026-04-14 11:53:49 +0530

    social-media-feed |aaded pq impl

--- social-media-feed/src/main/java/service/FeedService.java
+++ social-media-feed/src/main/java/service/FeedService.java
@@ -56,4 +56,50 @@ public class FeedService {
 
 
     }
+
+    public List<Post> getFeedPriorityQueue(String userId, int limit, LocalDateTime cursor) {
+
+        boolean userExists = userMgr.existById(userId);
+
+        if(!userExists) {
+            throw new UserNotFoundException("USer cannot be found with user id "  + userId);
+        }
+
+        //1. Find all followers and find all posts by follower sort by TimeStamp and return top limit
+        Set<String> following = followMgr.findById(userId);
+        if(following.isEmpty()) {
+            return List.of();
+        }
+
+        //2. Get all post by follower and save it in a DS sort by timestamp
+        //PriorityQueue<Post> postQueue = new PriorityQueue<>(Comparator.comparing(Post::getPostedTime).reversed());
+        //PriorityQueue<Post> posts = new PriorityQueue<>((a,b) -> b.getPostedTime().compareTo(a.getPostedTime()));
+        List<Post> allPosts = new ArrayList<>();
+
+        for(String followingId : following) {
+            List<Post> posts = postMgr.findByPostedBy(followingId);
+            allPosts.addAll(posts);
+
+        }
+
+        PriorityQueue<Post> pq = new PriorityQueue<>(Comparator.comparing(Post::getPostedTime));
+
+        for(Post post : allPosts) {
+
+            if(pq.size() < limit) {
+                pq.offer(post);
+            } else if (pq.peek().getPostedTime().isBefore(post.getPostedTime())){
+                pq.poll();
+                pq.offer(post);
+
+            }
+        }
+
+        List<Post> recentPosts = new ArrayList<>(pq);
+        recentPosts.sort(Comparator.comparing(Post::getPostedTime).reversed());
+
+        return recentPosts;
+
+
+    }
 }
